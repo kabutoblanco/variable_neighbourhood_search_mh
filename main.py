3335e7423bac6c01bbd39b071b8b4fa7a07c0365
kabutoblanco <mdquilindo@unicauca.edu.co>
2020-05-26 01:06:04 -0500

    statitstics corregido

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from metaheuristics.simplestate.hillclimbing.random_search import RandomSearch
 from metaheuristics.simplestate.vns.vns import VNS
 from utils.statistics import Statistics
 
+import copy
 import random
 
 def main():
@@ -16,15 +17,17 @@ def main():
         k = Knapsack(name_file)
         hcc = HillclimbingClassic()
         hcm = RandomSearch()
-        vns = VNS(random.randint(2, k.total_items))
+        vns = VNS(random.randint(2, int(k.total_items / 2 + 1)))
         algorithms = []
         algorithms.append(hcc)
         algorithms.append(hcm)
         algorithms.append(vns)
-        hcc.max_efos = 500
-        hcm.max_efos = 500
+        hcc.max_efos = 50
+        hcm.max_efos = 50
         information = [0] * 2
+        sublist_statistics = [0] * len(algorithms)
         print(name_file)
+        j = 0
         for algorithm in algorithms:
             vector = []
             successfull_count = 0
@@ -37,12 +40,14 @@ def main():
             information[1] = vector
             statistics.set_vector(information)
             statistics.successfull_count = successfull_count
+            sublist_statistics[j] = statistics
             i += 1
+            j += 1
             print(algorithm)
             print("Promedio: {}".format(statistics.average()))
             print("Desviación: {}".format(statistics.std()))
             print("Tasa de exito: {}".format(statistics.successfull_rate()))
-
+        list_statistics.append(copy.deepcopy(sublist_statistics))
     
 if __name__ == "__main__":
     main()
