b6faf5e86a627d15b2d866c57cbc55f45d8caa06
aalexvi <andyvilla@unicauca.edu.co>
2020-05-25 22:40:16 -0500

    init export class

--- data/export.py
+++ data/export.py
@@ -1,3 +1,11 @@
 class Export:
+
     def __init__(self):
+        self.filename = "dataExport.txt"
+    
+    def write(self, object, type):
+        if type == 0:
+            pass
         pass
+
+        
