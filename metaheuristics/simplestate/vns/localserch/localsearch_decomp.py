e6b483e17620bbc6f7e524e05554c00c295de290
aalexvi <andyvilla@unicauca.edu.co>
2020-05-27 08:47:11 -0500

    fixed export class and structure

--- metaheuristics/simplestate/vns/localserch/localsearch_decomp.py
+++
@@ -1,9 +0,0 @@
-from .localsearch import Localsearch
-
-class LocalsearchDecomp(Localsearch):
-
-    def execute(self, obj_neighborhood):
-        pass
-
-    def doSomething(self):
-        print(self.value * 3)
